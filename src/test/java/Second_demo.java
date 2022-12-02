f2dff5da0c840fb62f94ab363cc6adee2504e1f0
shondhi <52061402+shondhi1232@users.noreply.github.com>
2022-12-03 04:39:18 +0600

    new

--- src/test/java/Second_demo.java
+++ src/test/java/Second_demo.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
 public class Second_demo {
+    WebDriver driver;
+    @Before
+    public  void setup(){
+        System.setProperty("webdriver.gecko.driver","./src/test/resources/geckodriver.exe");
+        FirefoxOptions ops = new FirefoxOptions();
+        ops.addArguments("--headed");
+        driver = new FirefoxDriver(ops);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+    @Test
+    public void submitForm(){
+        driver.get("https://demoqa.com/text-box");
+        driver.findElement(By.id("userName")).sendKeys("shondi Akter");
+        driver.findElement(By.id("userEmail")).sendKeys("shondi@test.com");
+        driver.findElement(By.cssSelector("[id=currentAddress]")).sendKeys("Dhaka, Bangladesh");
+        driver.findElement(By.cssSelector("[id = permanentAddress]")).sendKeys("Dhaka, Bangladesh");
+        driver.findElement(By.id("submit")).click();
+    }
 }
