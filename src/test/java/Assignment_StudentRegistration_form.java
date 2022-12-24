098e4a70bb5d8a106470bf9beccb4adb1146dc30
shondhi <52061402+shondhi1232@users.noreply.github.com>
2022-12-25 00:16:07 +0600

    update

--- src/test/java/Assignment_StudentRegistration_form.java
+++ src/test/java/Assignment_StudentRegistration_form.java
@@ -10,8 +10,6 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Assignment_StudentRegistration_form {
 
@@ -33,10 +31,21 @@ public class Assignment_StudentRegistration_form {
         driver.findElement(By.id("lastName")).sendKeys("Akter");
         driver.findElement(By.id("userEmail")).sendKeys("shondi@test.com");
 
+    }
+    @Test
+    public void select_RadioButton(){
+        driver.get("https://demoqa.com/automation-practice-form");
         driver.findElement(By.xpath("//label[text()='Female']")).click();
 
+    }
+    @Test
+    public void enterPhoneNumber(){
+        driver.get("https://demoqa.com/automation-practice-form");
         driver.findElement(By.id("userNumber")).sendKeys("01335932998");
-
+    }
+    @Test
+    public void selectDateOfBirth(){
+        driver.get("https://demoqa.com/automation-practice-form");
         driver.findElement(By.id("dateOfBirthInput")).click();
         WebElement dropdown1 = driver.findElement(By.className("react-datepicker__month-select"));
         Select option = new Select(dropdown1);
@@ -46,8 +55,16 @@ public class Assignment_StudentRegistration_form {
         Select option2 = new Select(dropdown2);
         option2.selectByVisibleText("2000");
         driver.findElement(By.xpath("//div[text()='1']")).click();
-
+    }
+    @Test
+    public void subjectWrite(){
+        driver.get("https://demoqa.com/automation-practice-form");
         driver.findElement(By.id("subjectsInput")).sendKeys("JavasScript");
+    }
+    @Test
+    public void selectCheckBox(){
+        driver.get("https://demoqa.com/automation-practice-form");
+
 
         //driver.findElement(By.id("hobbies-checkbox-1")).click();
 
@@ -57,11 +74,29 @@ public class Assignment_StudentRegistration_form {
 
         //Selecting the last check box
         driver.findElement(By.cssSelector("label[for='hobbies-checkbox-3']")).click();
+    }
+    @Test
+    public void uploadImage(){
 
+        driver.get("https://demoqa.com/automation-practice-form");
         driver.findElement(By.id("uploadPicture")).sendKeys("C:\\baby.jpg");
-        driver.findElement(By.id("currentAddress")).sendKeys("Dhaka,Bangladesh");
+    }
+    @Test
+    public void writeCurrentAddress(){
 
+        driver.get("https://demoqa.com/automation-practice-form");
+        driver.findElement(By.id("currentAddress")).sendKeys("Dhaka,Bangladesh");
+    }
+
+    public void selectSateAndCity(){
+        driver.get("https://demoqa.com/automation-practice-form");
+            //not work
         driver.findElement(By.xpath("//div[text()='Rajasthan']")).click();
+
+    }
+    @Test
+    public void submitButton(){
+        driver.get("https://demoqa.com/automation-practice-form");
 
         driver.findElement(By.id("submit")).click();
     }
